22a76a19305124d7ebaefd168922444bcc9017f7
Princessbolade <mobolade.akinlua@outlook.com>
2021-01-18 21:17:09 +0100

    changes

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 document.getElementById('about').scrollIntoView({behavior: "smooth", block: "start", inline: "end"});
+document.getElementById('portfolio').scrollIntoView({behavior: "smooth", block: "start", inline: "end"});
+document.getElementById('experience').scrollIntoView({behavior: "smooth", block: "start", inline: "end"});
