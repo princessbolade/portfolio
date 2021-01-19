b9789ad860d66a46c57c06bd1e3cc6e69c7b2516
Princessbolade <mobolade.akinlua@outlook.com>
2021-01-19 09:37:49 +0100

    scroll into view

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-document.getElementById('about').scrollIntoView({behavior: "smooth", block: "start", inline: "end"});
-document.getElementById('portfolio').scrollIntoView({behavior: "smooth", block: "start", inline: "end"});
-document.getElementById('experience').scrollIntoView({behavior: "smooth", block: "start", inline: "end"});
+document.getElementById("about-nav").addEventListener("click",()=>{
+    document.getElementById('about').scrollIntoView({behavior: "smooth", block: "start", inline: "end"});
+})
+document.getElementById("port-nav").addEventListener("click",()=>{
+    document.getElementById('portfolio').scrollIntoView({behavior: "smooth", block: "start", inline: "end"});
+})
+document.getElementById("exp-nav").addEventListener("click",()=>{
+    document.getElementById('experience').scrollIntoView({behavior: "smooth", block: "start", inline: "end"});
+})
+
+
+
+ 
