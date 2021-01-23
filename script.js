7f03a062ae2f7ed978d99ca3a228c1e443dd2701
Princessbolade <mobolade.akinlua@outlook.com>
2021-01-23 16:59:35 +0100

    hamburger menu and svgs

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-document.getElementById("about-nav").addEventListener("click",()=>{
-    document.getElementById('about').scrollIntoView({behavior: "smooth", block: "start", inline: "end"});
-})
-document.getElementById("port-nav").addEventListener("click",()=>{
-    document.getElementById('portfolio').scrollIntoView({behavior: "smooth", block: "start", inline: "end"});
-})
-document.getElementById("exp-nav").addEventListener("click",()=>{
-    document.getElementById('experience').scrollIntoView({behavior: "smooth", block: "start", inline: "end"});
-})
-
-
-
- 
+document.getElementById("about-nav").addEventListener("click", () => {
+  document
+    .getElementById("about")
+    .scrollIntoView({ behavior: "smooth", block: "start", inline: "end" });
+});
+document.getElementById("port-nav").addEventListener("click", () => {
+  document
+    .getElementById("portfolio")
+    .scrollIntoView({ behavior: "smooth", block: "start", inline: "end" });
+});
+document.getElementById("exp-nav").addEventListener("click", () => {
+  document
+    .getElementById("experience")
+    .scrollIntoView({ behavior: "smooth", block: "start", inline: "end" });
+});
